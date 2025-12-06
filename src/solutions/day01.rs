aaf81321f58dfa1a7e874d7b2164f6bb6c9d8040
Marcel <marcel@local>
2025-12-06 16:34:55 +0100

    Update

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,10 +1,37 @@
+use anyhow::anyhow;
+use regex::Regex;
+
 use crate::solutions::Solution;
 
 pub struct Day01;
 
 impl Solution for Day01 {
-    fn part1(&self, _input: &str) -> anyhow::Result<String> {
-        todo!("Implement part 1")
+    fn part1(&self, input: &str) -> anyhow::Result<String> {
+        let line_regex = Regex::new(r"(L|R)(\d+)")?;
+
+        let mut hit_zero = 0;
+        let mut current = 50;
+
+        for line in input.lines() {
+            let captures = line_regex
+                .captures(line)
+                .ok_or(anyhow!("Could not match regex"))?;
+
+            let direction = &captures[1];
+
+            let number = captures[2].parse::<i32>()?;
+            let number = if direction == "L" { -number } else { number };
+
+            current = current + number;
+            current = current % 100;
+            current = if current < 0 { current + 100 } else { current };
+
+            if current == 0 {
+                hit_zero += 1;
+            }
+        }
+
+        return Ok(hit_zero.to_string());
     }
 
     fn part2(&self, _input: &str) -> anyhow::Result<String> {
@@ -33,7 +60,7 @@ mod tests {
             L82
         "};
         let result = Day01.part1(input)?;
-        assert_eq!(result, "expected answer");
+        assert_eq!(result, "3");
         Ok(())
     }
 }
